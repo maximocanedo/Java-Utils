d2e7a70bb3e9d796b7b6a78a016e19bdd4743fbb
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-10-09 21:26:57 -0300

    Clase con funciones misceláneas

---
+++ src/max/Misc.java
@@ -0,0 +1,17 @@
+package max;
+
+/**
+ * Clase con funciones útiles y variadas.
+ * @author maximocanedo
+ */
+public class Misc {
+	
+	/**
+	 * Imprime una línea de texto por consola.
+	 * @param msg El mensaje a imprimir.
+	 */
+	public static void out(Object msg) {
+		System.out.println(msg);
+	}
+
+}
